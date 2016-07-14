4f5e12080097951f65e007465ff254bfad5aa93e
George Rappel <george.concei@hotmail.com>
2016-07-14 13:45:05 -0300

    Update update_freedns.sh

--- MotionDetection/update_freedns.sh
+++ MotionDetection/update_freedns.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #FreeDNS updater script
 
-wget -q --read-timeout=0.0 --waitretry=5 --tries=2 --background http://freedns.afraid.org/dynamic/update.php?SWJTV1p5ZE5Mc3hQYThpbzY3c0JTVm12OjE1ODMwMDM2 -O /dev/null
+wget -q --read-timeout=0.0 --waitretry=5 --tries=2 --background http://freedns.afraid.org/dynamic/update.php?[UPDATE_KEY] -O /dev/null
