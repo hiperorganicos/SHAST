f27e1bfed9e59c485286a71ed73a7452d15cfec2
georgerappel <george.concei@hotmail.com>
2017-05-02 11:23:06 -0300

    Mudar IP Arduino Sketch

---
+++ Arduino/Code/shast_yun_osc/shast_yun_osc.ino
@@ -0,0 +1,63 @@
+#include <dht11.h>
+#include <yunOSC.h>
+#include <Process.h>
+
+#define IP_ADR "146.164.9.237"
+//#define IP_ADR "10.32.0.37"
+#define PORT 22244
+
+//pins
+#define DHT11_PIN 7
+
+int LDR_PIN = A2;
+int MOI_PIN = A0;
+int DHT_PIN = 7; //Pino DATA do Sensor ligado na porta Analogica A1
+
+
+dht11 DHT;
+
+//storage vals
+int ldr = 0;
+int moi = 0;
+int chk;
+float temp = 0;
+float humd = 0;
+
+
+void setup() {
+  // Initialize Bridge
+  Bridge.begin();
+  
+  // make udp connection
+  osc.begin(IP_ADR, PORT);
+  
+  delay(1000);
+}
+
+void loop() {
+  ldr = analogRead(LDR_PIN);
+  moi = analogRead(MOI_PIN);
+  chk = DHT.read(DHT11_PIN);
+  temp = DHT.temperature;
+  humd = DHT.humidity;
+  
+  runSender("/shast/luz", ldr);
+  delay(350);
+  
+  runSender("/shast/umidadeSolo", moi);
+  delay(350);
+
+  runSender("/shast/temperatura", (int)temp);
+  delay(350);
+
+  runSender("/shast/umidadeAr", (int)humd);
+  delay(350);
+
+  delay(100);
+}
+
+void runSender(String addr, int val) {
+  int values[1];
+  values[0] = val;
+  osc.send(addr, values, 1);
+}
